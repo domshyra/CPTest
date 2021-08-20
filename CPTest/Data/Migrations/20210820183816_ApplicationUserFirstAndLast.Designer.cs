d8105cc87d20ebde0c294089586ef75398623c28
Dominic Schira <DominicS@wsrb.com>
2021-08-20 11:38:45 -0700

    Added migration

--- CPTest/Data/Migrations/20210820183816_ApplicationUserFirstAndLast.Designer.cs
+++ CPTest/Data/Migrations/20210820183816_ApplicationUserFirstAndLast.Designer.cs
@@ -4,14 +4,16 @@ using CPTest.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CPTest.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210820183816_ApplicationUserFirstAndLast")]
+    partial class ApplicationUserFirstAndLast
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
